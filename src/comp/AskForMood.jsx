433a6b4bc3d2f93834603b347742e6380ad9c0f1
NeelthebigGuy <nkhiroya33@gmail.com>
2024-02-01 17:17:06 -0400

    faded out on click by number

--- src/comp/AskForMood.jsx
+++ src/comp/AskForMood.jsx
@@ -11,6 +11,25 @@ export default function AskForMood() {
 
   function HandleClick(mood){
     setmood(mood);
+
+    if(mood == 2){
+      document.getElementById("happyselect").style.transition = ".1s";
+    }
+
+    if(mood == 1){
+      document.getElementById("mehselect").style.transition = ".1s";
+    }
+
+    if(mood == 0){
+      document.getElementById("sadselect").style.transition = ".1s";
+    }
+    
+    document.getElementById("mehselect").style.opacity = "0";
+    document.getElementById("happyselect").style.opacity = "0";
+    document.getElementById("sadselect").style.opacity = "0";
+    document.getElementById("selectionpage").style.opacity = "0";
+    document.getElementById("selectionpage").style.visibility = "hidden";
+
     setMoodSelectPageVisiabily(false);
   }
 
@@ -26,21 +45,27 @@ export default function AskForMood() {
 
   return (
     <>
-    {<div className={noSelectedMood ? "full-height" : "fadeout"}>
+    <div id="selectionpage" className="full-height">
           <div className='conatiner'>
               <div className='howwasyourdaybackground'>
                   <div className='howwasyourday bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent md:text-[40px]'>How Was Your Mood Today?<div className='myline bg-gradient-to-r from-amber-500 to-pink-500 md:mt-2'/></div>
               </div>
+              
               <div className='selectioncontainer mt-[50px]'>
-              <Faces theclass="fa-regular fa-face-smile text-[100px]" moodnumber='2'/>
-              <Faces theclass="fa-regular fa-face-meh text-[100px]" moodnumber='1'/>
-              <Faces theclass="fa-regular fa-face-frown text-[100px]" moodnumber='0'/>
-              </div>
-              <div>
-                {mood}
+                <div id="happyselect" className='happyseletiont'>
+                  <Faces theclass="fa-regular fa-face-smile text-[100px]" moodnumber='2'/>
+                </div>
+                
+                <div id="mehselect" className='mehselectiont'>
+                  <Faces theclass="fa-regular fa-face-meh text-[100px]" moodnumber='1'/>
+                </div>
+
+                <div id="sadselect" className='sadselectiont'>
+                  <Faces theclass="fa-regular fa-face-frown text-[100px]" moodnumber='0'/>
+                </div>
               </div>
           </div>
-      </div>}
+      </div>
     </>
   )
 }
